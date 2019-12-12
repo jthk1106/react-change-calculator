5fcffc5b568c044f8011f29dacd93082bcc4c8d3
jthk1106 <thk116@gmail.com>
2019-12-12 23:09:54 +0000

    calculations all good

--- src/app.jsx
+++ src/app.jsx
@@ -37,10 +37,120 @@ class App extends Component {
   }
 
   calculate() {
+
+    // TOTAL CHANGE
+    const changeDue = (this.state.received - this.state.due)
     this.setState({
-      change: this.state.received - this.state.due
+      change: changeDue
     })
 
+    let dollaz = Math.floor(changeDue)
+    let cense = Math.floor((changeDue % 1) * 100)
+    console.log(`change: ${changeDue}, dollaz: ${dollaz}, cense: ${cense}`)
+
+    // TWENTIES
+    if(dollaz%20 === 0) {
+        this.setState({
+          twenties: dollaz/20
+        })
+        dollaz = 0;
+    } else {
+        this.setState({
+          twenties: Math.floor(dollaz/20)
+        })
+        dollaz = dollaz%20;
+    };
+
+    // TENS
+    if(dollaz%10 === 0) {
+        this.setState({
+          tens: dollaz/10
+        })
+        dollaz = 0;
+    } else {
+        this.setState({
+          tens: Math.floor(dollaz/10)
+        })
+        dollaz = dollaz%10;
+    };
+
+    // FIVES
+    if(dollaz%5 === 0) {
+        this.setState({
+          fives: dollaz/5
+        })
+        dollaz = 0;
+    } else {
+        this.setState({
+          fives: Math.floor(dollaz/5)
+        })
+        dollaz = dollaz%5;
+    };
+
+    // ONES
+    if(dollaz < 5) {
+        // pennies = cense;
+        this.setState({
+          ones: dollaz
+        })
+    }
+
+    console.log(`cense before everything: ${cense}`)
+    // QUARTERS
+    if(cense%25 === 0) {
+        // quarters = cense/25;
+        this.setState({
+          quarters: cense/25
+        })
+        cense = 0;
+    } else {
+        // quarters = Math.floor(cense/25);
+        this.setState({
+          quarters: Math.floor(cense/25)
+        })
+        cense = cense%25;
+    };
+    console.log(`cense after quarters check: ${cense}`)
+
+    // DIMES
+    if(cense%10 === 0) {
+        //
+        this.setState({
+          dimes: cense/10
+        })
+        cense = 0;
+    } else {
+        // dimes = Math.floor(cense/10);
+        this.setState({
+          dimes: Math.floor(cense/10)
+        })
+        cense = cense%10;
+    };
+    console.log(`cense after dimes check: ${cense}`)
+
+    // NICKELS
+    if(cense%5 === 0) {
+        //
+        this.setState({
+          nickels: cense/5
+        })
+        cense = 0;
+    } else {
+        // nickels = Math.floor(cense/5);
+        this.setState({
+          nickels: Math.floor(cense/5)
+        })
+        cense = cense%5;
+    };
+    console.log(`cense after nickels check: ${cense}`)
+
+    // PENNIES
+    if(cense < 5) {
+        // pennies = cense;
+        this.setState({
+          pennies: cense
+        })
+    }
   }
 
   render() {
@@ -53,7 +163,7 @@ class App extends Component {
         </div>
         <div className='row justify-content-around'>
           <div className='col-sm-4 leftSide'>
-            <div className="panel panel-default">
+            <div className="panel panel-default align-middle">
               <div className='panel-heading'>Enter Information</div>
               <div className="panel-body">
                 <label>How much is due?</label>
